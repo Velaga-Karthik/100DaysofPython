e9900922392beabc28274e1f417072dd47bc4c99
Velaga-Karthik <167983771+Velaga-Karthik@users.noreply.github.com>
2025-05-22 00:08:44 +0530

    Update Day_3_Project_Treasure_Island.py

--- Day3_Project_Treasure_Island/Day_3_Project_Treasure_Island.py
+++ Day3_Project_Treasure_Island/Day_3_Project_Treasure_Island.py
@@ -1,5 +1,4 @@
 from Design import logo
-print(logo)
 print("Welcome to Treasure Island.")
 print("Your mission is to find the treasure.")
 choice_1 = input('You\'re at a cross road. Where do you want to go? Type "left" or "right" \n').lower()
@@ -8,7 +7,8 @@ if choice_1=="left":
     if choice_2=="wait":
         choice_3=input('You arrive at the island unharmed. There is a house with 3 doors.One red, one yellow and one blue. Which colour do you choose? \n').lower()
         if choice_3=="yellow":
-            print("You Win!")
+            print("You Win! You Found the Treasure")
+            print(logo)
         elif choice_3=="red":
             print("Burned by fire.Game Over.")
         elif choice_3=="blue:":
